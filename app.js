1d1b1745132ac934de7564eb49ac07bdc5e8ef55
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-11-24 14:08:30 +0100

    Life goes on

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 const user = require('./users.json');
 const fs = require('fs');
+const http = require('http');
+const express = require('express');
 
-userStream = fs.createWriteStream('./users.json');
-loluser = user[0].lNAme='Alexander';
-userStream.write(user);
+const app = express();
+const port = 3000;
 
-console.log(loluser);
+const statusMessage ='all good, hehe';
+app.get('/', (req, res) => {
+    res.writeHead(200,[statusMessage]);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
+
+/*  EDIT FILE
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE 
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE   
+    EDIT FILE*/ 
+fs.readFile('users.json', 'utf8', function(err, data){
+
+const newValue = data.replace(user[0].fName, 'mario');
+
+fs.writeFile('users.json', newValue, function(){
+
+console.log(newValue);
+
+});
+
+});
+
+console.log('You have modified the file ');
+/*  EDIT FILE
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE 
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE
+    EDIT FILE   
+    EDIT FILE*/ 
