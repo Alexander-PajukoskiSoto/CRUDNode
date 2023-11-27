9197989fc5f6fa45e862936d3a8d36c5a06f77f5
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-11-27 12:25:39 +0100

    Adding works + checker consts

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const user = require('./users.json');
 const fs = require('fs');
 const http = require('http');
 const express = require('express');
+const { json } = require('body-parser');
+editChecker=false;
+addChecker= true;
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-const statusMessage ='all good, hehe';
-app.get('/', (req, res) => {
-    res.writeHead(200,[statusMessage]);
-})
+// const statusMessage ='all good, hehe';
+// app.get('/', (req, res) => {
+//     res.writeHead(200,[statusMessage]);
+// })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// })
 
 /*  EDIT FILE
     EDIT FILE
@@ -27,19 +30,18 @@ app.listen(port, () => {
     EDIT FILE
     EDIT FILE   
     EDIT FILE*/ 
-fs.readFile('users.json', 'utf8', function(err, data){
+    if(editChecker==true){fs.readFile('users.json', 'utf8', function(err, data){
 
-const newValue = data.replace(user[0].fName, 'mario');
+        const newValue = data.replace(user[0].fName, 'luigii');
+        
+        fs.writeFile('users.json', newValue, function(){
+        
+        // console.log(newValue);
+        
+        });
+        
+        });}
 
-fs.writeFile('users.json', newValue, function(){
-
-console.log(newValue);
-
-});
-
-});
-
-console.log('You have modified the file ');
 /*  EDIT FILE
     EDIT FILE
     EDIT FILE
@@ -52,3 +54,30 @@ console.log('You have modified the file ');
     EDIT FILE
     EDIT FILE   
     EDIT FILE*/ 
+
+// ADD NEW USER
+// ADD NEW USER
+// ADD NEW USER
+// ADD NEW USER
+// ADD NEW USER
+
+if(addChecker==true){fs.readFile('users.json', 'utf8', function(err, data){
+
+    newFName = 'Dedan';
+    newLName = 'Doodan';
+    userList = JSON.parse(data);
+
+    userList.push({fName: newFName,lName: newLName});
+
+    const stringUserList =JSON.stringify(userList);
+    console.log(stringUserList);
+    fs.writeFile('users.json', stringUserList, function(){
+
+        // console.log(newValue);
+        
+        });
+    
+})}
+
+
+
